d539029b4c7457b97ca409ac919de16ee265c040
vrdst <evrdst@interia.pl>
2016-05-23 02:51:47 +0200

    tworzenie pdf

--- task1EmailApp/src/servlets/EmailApp.java
+++ task1EmailApp/src/servlets/EmailApp.java
@@ -1,15 +1,26 @@
 package servlets;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 //@author endrju
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 
 public class EmailApp extends HttpServlet {
 
@@ -17,41 +28,58 @@ public class EmailApp extends HttpServlet {
 	 * serialization
 	 */
 	private static final long serialVersionUID = -3585248524522011431L;
+	
+	Locale locale = new Locale("en", "US");
 
+	ResourceBundle labels = ResourceBundle.getBundle("i18n.errors", locale);
+	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
 		Map<String, String> formErrors = new HashMap<String, String>();
-		String email;
 		
-		if(!request.getParameter("formName").matches("[a-zA-Z]+")){
-			formErrors.put("name", "name should contain only letters");
-			System.out.println("name error");
-		}
-		if(!request.getParameter("formSurname").matches("[a-zA-Z]+")){
-			formErrors.put("surname", "surname should contain only letters");
-			System.out.println("surname error");
-		}
-		if(!request.getParameter("formPhoneNo").matches("[0-9]+")){
-			formErrors.put("phone", "phone number should contain only digits");
-			System.out.println("phone error");
-		}
+		String name = request.getParameter("formName");
+		String surname = request.getParameter("formSurname");
+		String phone = request.getParameter("formPhoneNo");
+		String email = request.getParameter("formEmailAddress");
 		
-		email = request.getParameter("formEmailAddress");		
+		if(!name.matches("[a-zA-Z]+")){
+			formErrors.put("name", labels.getString("nameError"));
+		}
+		if(!surname.matches("[a-zA-Z]+")){
+			formErrors.put("surname", labels.getString("surnameError"));
+		}
+		if(!phone.matches("[0-9]+")){
+			formErrors.put("phone", labels.getString("phoneNoError"));
+		}
+			
 		if(!(email.contains("@") && (email.split("@")[1].length() - email.split("@")[1].replace(".", "").length()) == 1)){
-			formErrors.put("email", "incorrect email format");
-			System.out.println("email error");
+			formErrors.put("email", labels.getString("emailError"));
 		}
 		
 	    if (formErrors.isEmpty()) {
-	        // fields correctly filled
-	        ;
+	        Document document = new Document();
+	        	
+            try {
+				PdfWriter.getInstance(document, new FileOutputStream("Contact.pdf"));
+	           		document.open();
+					document.add(new Paragraph("Name: " + name));
+					document.add(new Paragraph("Surname: " + surname));
+					document.add(new Paragraph("Phone: " + phone));
+					document.add(new Paragraph("Email: " + email));
+				} catch (DocumentException e) {
+					e.printStackTrace();
+				}	catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				}
+
+	            document.close();
 	    } else {
-	        // Put errors in request scope and forward back to JSP.
+	        // map errors and send back to index
 	        request.setAttribute("formErrors", formErrors);
 	        request.getRequestDispatcher("index.jsp").forward(request, response);
 	    }
 
 	}
-
 }
